13f9ce76d1118346c15d2845e209eb801200f136
Ian Gutwinski <iangutwinski@gmail.com>
2019-02-03 15:03:01 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -1,6 +1,6 @@
 # birthday_kids = {
-#   "Timmy" => 9, 
-#   "Sarah" => 6, 
+#   "Timmy" => 9,
+#   "Sarah" => 6,
 #   "Amanda" => 27
 # }
 
@@ -8,5 +8,3 @@ def happy_birthday(birthday_kids)
   birthday_kids.each do |key, variable|
     puts "Happy Birthday #{key}! You are now #{variable} years old!"
 end
-
-
